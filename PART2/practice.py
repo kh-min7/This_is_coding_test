8266a7d8dd759d81b02710eae6d780fe108593ed
kh-min7 <rudgh2879@naver.com>
2021-12-11 23:36:01 +0900

    .

--- PART2/practice.py
+++ PART2/practice.py
@@ -1,10 +1,24 @@
 from collections import deque
 
-n, m, k, x = map(int, input().split())
+n = int(input())
+k = int(input())
 
-graph = [[] for _ in range(n + 1)]
+graph = [[] * (n + 1) for _ in range(n + 1)]
 
-for _ in range(m):
+for _ in range(k):
     a, b = map(int, input().split())
     graph[a].append(b)
+    graph[b].append(a)
 
+visited = [0] * (n + 1)
+
+
+def dfs(start):
+    visited[start] = 1
+    for i in graph[start]:
+        if visited[i] == 0:
+            dfs(i)
+    return sum(visited) - 1
+
+
+print(dfs(1))
