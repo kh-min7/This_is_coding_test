b1696953f5f14535759ad3ad448653fab2a2c71a
kh-min7 <rudgh2879@naver.com>
2021-12-16 23:56:16 +0900

    .

--- PART3/Binary_search_3.py
+++ PART3/Binary_search_3.py
@@ -1,5 +1,30 @@
-n, c = map(int, input().split())
+import sys
+
+ipt = sys.stdin.readline
+
+n, c = map(int, ipt().split())
 array = []
 for _ in range(n):
-    array.append(int(input()))
+    array.append(int(ipt()))
+array.sort()
 
+start = array[1] - array[0]
+end = array[-1] - array[0]
+result = 0
+
+while start <= end:
+    mid = (start + end) // 2
+    value = array[0]
+    cnt = 1
+    for i in range(1, n):
+        if array[i] >= value + mid:
+            value = array[i]
+            cnt += 1
+
+    if cnt >= c:
+        start = mid + 1
+        result = mid
+    else:
+        end = mid - 1
+
+print(result)
