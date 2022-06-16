46567cd9458becfa4e85881965e4d76c3fca404c
kh-min7 <rudgh2879@naver.com>
2022-06-16 20:48:15 +0900

    .

--- 1.py
+++ 1.py
@@ -1,3 +1,18 @@
-def solution(N, stages):
-    answer = []
-    length = len(stages)
+a, b = map(int, input().split())
+c = int(input())
+
+def clock(a, b, c):
+  b += c
+  if b >= 60:
+    d = b // 60
+    e = b % 60
+  
+    b = e
+    a += d
+
+  if a > 24:
+    a -= 24
+  
+  print(a, b)
+
+clock(a, b, c)
